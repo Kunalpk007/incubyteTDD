b2ca82ebad57ab47c937f9c564a661421e970848
Kunal Kadam <kunalpk007@gmail.com>
2024-10-21 13:14:20 +0530

    single string valued parsed to number to pass the test

--- src/addNumber.ts
+++ src/addNumber.ts
@@ -2,11 +2,11 @@ export function add(input: string) {
     if (input === '') return 0; // Return 0 for empty input
 
       // Split the input into individual numbers by comma
-      const numbers = input.split(',');
-      let parsedResult =  numbers.map(str => {
-        const parsedNumber = parseFloat(str);
-        return parsedNumber;
-    });
+        const numbers = input.split(',');
+        const parsedNumber = parseFloat(numbers[0]);
+        
+    
+    
 
-return parsedResult;
+return parsedNumber;
 }
